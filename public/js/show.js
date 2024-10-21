f2b6f58e0ab3ae3937e6707e75f544fbe47ceccb
Md. Raian Hossain <raiansanil@gmail.com>
2024-10-21 15:12:06 +0600

    session ad popup ad

--- public/js/show.js
+++ public/js/show.js
@@ -1,3 +1,6 @@
+
+
+import { HlsJsP2PEngine } from "p2p-media-loader-hlsjs";
 import { API_BASE_URL } from './constant.js';
 // Function to get the stream ID from URL query parameter
 function getStreamIdFromURL() {
@@ -24,14 +27,44 @@ async function fetchStreamData(streamId) {
 function updateUIWithStreamData(streamData) {
   // Update video player
   const videoPlayer = document.getElementById('videoPlayer');
-  const videoSource = document.getElementById('videoSource');
-  const videoTitle = document.getElementById('videoTitle');
+  // const videoSource = document.getElementById('videoSource');
+  // const videoTitle = document.getElementById('videoTitle');
 
-  videoSource.src = `${API_BASE_URL}/assets/videos/${streamData.stream}`;
+  videoPlayer.src = streamData.stream;
   videoTitle.textContent = streamData.title;
 
-  // Load the new video source
-  videoPlayer.load();
+  // // Load the new video source
+  // videoPlayer.load();
+
+  //
+  const player = document.querySelector("media-player");
+  // Inject P2P capabilities into Hls.js
+  const HlsWithP2P = HlsJsP2PEngine.injectMixin(window.Hls);
+
+  player.addEventListener("provider-change", (event) => {
+    const provider = event.detail;
+
+    // Check if the provider is HLS
+    if (provider?.type === "hls") {
+      provider.library = HlsWithP2P;
+
+      provider.config = {
+        p2p: {
+          core: {
+            swarmId: "test893648527354",
+            // other P2P engine config parameters go here
+          },
+          onHlsJsCreated: (hls) => {
+            hls.p2pEngine.addEventListener("onPeerConnect", (params) => {
+              console.log("Peer connected:", params.peerId);
+            });
+            // Subscribe to P2P engine and Hls.js events here
+          },
+        },
+      };
+    }
+  });
+  //
 
   // Update ads dynamically
   const ad1Container = document.getElementById('ad1');
@@ -71,6 +104,8 @@ async function init() {
 
   if (streamData) {
     updateUIWithStreamData(streamData);
+    handleSessionAd(streamData.sessionAd);
+    handlePopupAds(streamData.popupAds);
   }
 }
 
@@ -79,8 +114,48 @@ async function init() {
 // Show modal 5 seconds after the session starts
 window.onload = function() {
   init();
-  setTimeout(function() {
-      var adModal = new bootstrap.Modal(document.getElementById('adModal'));
-      adModal.show();
-  }, 1000);
+  // setTimeout(function() {
+  //     var adModal = new bootstrap.Modal(document.getElementById('adModal'));
+  //     adModal.show();
+  // }, 1000);
 };
+
+function showAdModal(adContent) {
+  document.querySelector('.modal-ad').innerHTML = adContent;
+  const adModal = new bootstrap.Modal(document.getElementById('adModal'));
+  adModal.show();
+}
+
+function handleSessionAd(sessionAd) {
+  const lastAdTime = localStorage.getItem('lastAdTime');
+  const now = Date.now();
+
+  if(!lastAdTime || now - lastAdTime > 24 * 60 * 60 * 1000) {
+    setTimeout(() => {
+      showAdModal(sessionAd);
+      localStorage.setItem('lastAdTime', now);
+    }, 1 * 60 * 1000);
+  }
+}
+
+function handlePopupAds(popupAds) {
+  // const creationTime = new Date(createdAt).getTime();
+  const creationTime = Date.now() - 1 * 60 * 1000;
+  const now = Date.now();
+
+  popupAds.forEach(popupAd => {
+    const popupAdTimeMs = parseInt(popupAd.popupAdTimeInterval) * 60 * 1000;
+    const adShowTime = creationTime + popupAdTimeMs;
+    console.log("now", now);
+    console.log("adShowTime", adShowTime);
+    if (now < adShowTime) {
+      createPopupAdTimeout(popupAd.popupAd, adShowTime - now);
+    }
+  })
+}
+
+function createPopupAdTimeout(adContent, delay) {
+  setTimeout(() => {
+    showAdModal(adContent);
+  }, delay); // Delay is in milliseconds
+}
