e09e3560bffef91adb019f0776cf25f99ab921ab
Md. Raian Hossain <raiansanil@gmail.com>
2024-10-27 01:47:10 +0600

    check

--- public/js/constant.js
+++ public/js/constant.js
@@ -1,6 +1,6 @@
 // Define a constant
-// export const API_BASE_URL = "https://stream.saleusa.shop";
-export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "https://stream.saleusa.shop";
+// export const API_BASE_URL = "http://localhost:3000";
 
 // You can also export multiple constants
 export const API_VERSION = "v1";
